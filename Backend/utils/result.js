47e4508eba3a8f18974785ad1be72ef5013c86c8
Harshal Gaulkar <harshalgaulkar04@gmail.com>
2026-06-04 22:02:06 +0530

    reducing the code rewriting for error management

--- Backend/utils/result.js
+++ Backend/utils/result.js
@@ -1,3 +1,6 @@
+// why we write these 
+// 1. to avoid writing the same code again and again
+// 2. to make the code more readable and maintainable
 function createResult(err, data) {
     const result = {}
     if (data) {
