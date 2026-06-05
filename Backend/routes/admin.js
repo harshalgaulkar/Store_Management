ab0459f391fec6924b5fdf4a7b8d506df41eea3c
Harshal Gaulkar <harshalgaulkar04@gmail.com>
2026-06-05 12:21:50 +0530

    Count of stores

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -57,12 +57,19 @@ router.post('/login', (req, res) => {
 
 
 // Count of users
-router.get('/count', (req, res) => {
+router.get('/users/count', (req, res) => {
     const sql = `SELECT COUNT(*) AS userCount FROM users WHERE role = "Normal"`
     pool.query(sql, (err, data) => {
         res.send(result.createResult(err, data[0]))
     })
 })
 
+// Count of stores
+router.get('/stores/count', (req, res) => {
+    const sql = `SELECT COUNT(*) AS storeCount FROM stores`
+    pool.query(sql, (err, data) => {
+        res.send(result.createResult(err, data[0]))
+    })
+})
 
 module.exports = router
