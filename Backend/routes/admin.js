f9e97ba2f779d2de7d6db89116500bfde185d07c
Harshal Gaulkar <harshalgaulkar04@gmail.com>
2026-06-05 10:12:55 +0530

    login API of Admin

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -22,4 +22,36 @@ router.post('/register', (req, res) => {
     })
 })
 
+// Admin login route
+router.post('/login', (req, res) => {
+    const { email, password } = req.body
+    const sql = `SELECT * FROM users WHERE email = ? AND role = 'Admin'`
+    pool.query(sql, [email], (err, data) => {
+        if (err)
+            res.send(result.createResult(err))
+        else if (data.length == 0)
+            res.send(result.createResult("Invalid Email or Not an Admin"))
+        else {
+            bcrypt.compare(password, data[0].password, (err, passwordStatus) => {
+                if (passwordStatus) {
+                    const payload = {
+                        uid: data[0].uid,
+                    }
+                    const token = jwt.sign(payload, config.SECRET)
+                    const user = {
+                        token,
+                        name: data[0].name,
+                        email: data[0].email,
+                        address: data[0].address,
+                        phone: data[0].phone,
+                        role: data[0].role
+                    }
+                    res.send(result.createResult(null, user))
+                }
+                else              
+                    res.send(result.createResult('Invalid Password'))
+            })
+        }
+    })
+})
 module.exports = router
