d89c0608208a9fb18312e8bed46e1fc45c81ba2d
Harshal Gaulkar <harshalgaulkar04@gmail.com>
2026-06-06 23:27:09 +0530

    fix: allow token header in CORS preflight

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,12 @@ const cors = require('cors')
 
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'token'],
+    credentials: true
+}))
 app.use(express.json())
 
 // Importing routes
